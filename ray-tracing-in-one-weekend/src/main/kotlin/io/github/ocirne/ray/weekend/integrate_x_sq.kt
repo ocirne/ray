60d0e56be1435f92c1cf639a50215c4af7e7bc8a
Enrico Pilz <kontakt@enricopilz.de>
2021-06-23 21:43:58 +0200

    Integrating with pdf

--- ray-tracing-in-one-weekend/src/main/kotlin/io/github/ocirne/ray/weekend/integrate_x_sq.kt
+++ ray-tracing-in-one-weekend/src/main/kotlin/io/github/ocirne/ray/weekend/integrate_x_sq.kt
@@ -1,13 +1,18 @@
 package io.github.ocirne.ray.weekend
 
+import kotlin.math.sqrt
 import kotlin.random.Random
+
+fun pdf(x: Double): Double {
+    return 0.5*x
+}
 
 fun main() {
     val N = 1_000_000
     var sum = 0.0
     for (i in 0 until N) {
-        val x = Random.nextDouble(0.0, 2.0)
-        sum += x * x
+        val x = sqrt(Random.nextDouble(0.0, 4.0))
+        sum += x * x / pdf(x)
     }
-    println("I = ${2 * sum/N}")
+    println("I = ${sum/N}")
 }
