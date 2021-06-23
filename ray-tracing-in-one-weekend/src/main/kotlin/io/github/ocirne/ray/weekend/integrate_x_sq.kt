7e681b88fa0b595351fc23c7604231c4317ae89c
Enrico Pilz <kontakt@enricopilz.de>
2021-06-23 21:50:17 +0200

    Integrating with knowing the answer

--- ray-tracing-in-one-weekend/src/main/kotlin/io/github/ocirne/ray/weekend/integrate_x_sq.kt
+++ ray-tracing-in-one-weekend/src/main/kotlin/io/github/ocirne/ray/weekend/integrate_x_sq.kt
@@ -1,16 +1,17 @@
 package io.github.ocirne.ray.weekend
 
+import kotlin.math.pow
 import kotlin.random.Random
 
 fun pdf(x: Double): Double {
-    return 0.5
+    return 3.0*x*x/8.0
 }
 
 fun main() {
-    val N = 1_000_000
+    val N = 1
     var sum = 0.0
     for (i in 0 until N) {
-        val x = Random.nextDouble(0.0, 2.0)
+        val x = pow(Random.nextDouble(0.0, 8.0), 1.0/3.0)
         sum += x * x / pdf(x)
     }
     println("I = ${sum/N}")
