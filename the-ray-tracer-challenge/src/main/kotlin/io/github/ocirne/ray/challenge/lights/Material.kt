764807c94ce8dd2b18d58dd5084039f7ded8169c
Enrico Pilz <kontakt@enricopilz.de>
2021-06-29 22:10:15 +0200

    Implement lighting method

--- the-ray-tracer-challenge/src/main/kotlin/io/github/ocirne/ray/challenge/lights/Material.kt
+++ the-ray-tracer-challenge/src/main/kotlin/io/github/ocirne/ray/challenge/lights/Material.kt
@@ -1,7 +1,7 @@
 package io.github.ocirne.ray.challenge.lights
 
-import io.github.ocirne.ray.challenge.tuples.Color
-import io.github.ocirne.ray.challenge.tuples.WHITE
+import io.github.ocirne.ray.challenge.tuples.*
+import kotlin.math.pow
 
 data class Material(
     val color: Color = WHITE,
@@ -9,4 +9,23 @@ data class Material(
     val diffuse: Double = 0.9,
     val specular: Double = 0.9,
     val shininess: Double = 200.0
-)
+) {
+    fun lighting(light: PointLight, point: Point, eyev: Vector, normalv: Vector): Color {
+        val effectiveColor = color * light.intensity
+        val lightv = (light.position - point).normalize()
+        val effectiveAmbient = effectiveColor * ambient
+        val lightDotNormal = lightv.dot(normalv)
+        var effectiveDiffuse = BLACK
+        var effectiveSpecular = BLACK
+        if (lightDotNormal >= 0) {
+            effectiveDiffuse = effectiveColor * diffuse * lightDotNormal
+            val reflectv = (-lightv).reflect(normalv)
+            val reflectDotEye = reflectv.dot(eyev)
+            if (reflectDotEye > 0) {
+                val factor = reflectDotEye.pow(shininess)
+                effectiveSpecular = light.intensity * specular * factor
+            }
+        }
+        return effectiveAmbient + effectiveDiffuse + effectiveSpecular
+    }
+}
--- the-ray-tracer-challenge/src/test/kotlin/io/github/ocirne/ray/challenge/MaterialTest.kt
+++ the-ray-tracer-challenge/src/test/kotlin/io/github/ocirne/ray/challenge/MaterialTest.kt
@@ -1,83 +1,75 @@
 package io.github.ocirne.ray.challenge
 
 import io.github.ocirne.ray.challenge.lights.Material
+import io.github.ocirne.ray.challenge.lights.PointLight
 import io.github.ocirne.ray.challenge.tuples.*
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
+import kotlin.math.sqrt
 
 internal class MaterialTest {
 
-     private val m = Material()
-     private val position = point(0, 0, 0)
+    private val magic2 = sqrt(2.0) / 2.0
 
-      @Test
-      fun `Scenario The default material`() {
-          val m = Material()
-          m.color shouldBe color(1, 1, 1)
-          m.ambient shouldBe 0.1
-          m.diffuse shouldBe 0.9
-          m.specular shouldBe 0.9
-          m.shininess shouldBe 200.0
-      }
-/*
-      @Test
-      fun `Scenario Reflectivity for the default material`() {
-  val m = Material()
-  m.reflective shouldBe 0.0
-}
+    private val m = Material()
+    private val position = point(0, 0, 0)
 
-      @Test
-      fun `Scenario Transparency and Refractive Index for the default material`() {
-  val m = Material()
-  m.transparency shouldBe 0.0
-    val m.refractive_index shouldBe 1.0
-}
+    @Test
+    fun `Scenario The default material`() {
+        val m = Material()
+        m.color shouldBe color(1, 1, 1)
+        m.ambient shouldBe 0.1
+        m.diffuse shouldBe 0.9
+        m.specular shouldBe 0.9
+        m.shininess shouldBe 200.0
+    }
 
-      @Test
-      fun `Scenario Lighting with the eye between the light and the surface`() {
-  val eyev = vector(0, 0, -1)
-    val normalv = vector(0, 0, -1)
-    val light = point_light(point(0, 0, -10), color(1, 1, 1))
-  val result = lighting(m, light, position, eyev, normalv)
-  result shouldBe color(1.9, 1.9, 1.9)
-}
+    @Test
+    fun `Scenario Lighting with the eye between the light and the surface`() {
+        val eyev = vector(0, 0, -1)
+        val normalv = vector(0, 0, -1)
+        val light = PointLight(point(0, 0, -10), color(1, 1, 1))
+        val result = m.lighting(light, position, eyev, normalv)
+        result shouldBe color(1.9, 1.9, 1.9)
+    }
 
-      @Test
-      fun `Scenario Lighting with the eye between light and surface, eye offset 45°`() {
-  val eyev = vector(0, √2/2, -√2/2)
-    val normalv = vector(0, 0, -1)
-    val light = point_light(point(0, 0, -10), color(1, 1, 1))
-  val result = lighting(m, light, position, eyev, normalv)
-  result shouldBe color(1.0, 1.0, 1.0)
-}
+    @Test
+    fun `Scenario Lighting with the eye between light and surface, eye offset 45 deg`() {
+        val eyev = vector(0.0, magic2, magic2)
+        val normalv = vector(0, 0, -1)
+        val light = PointLight(point(0, 0, -10), color(1, 1, 1))
+        val result = m.lighting(light, position, eyev, normalv)
+        result shouldBe color(1.0, 1.0, 1.0)
+    }
 
-      @Test
-      fun `Scenario Lighting with eye opposite surface, light offset 45°`() {
-  val eyev = vector(0, 0, -1)
-    val normalv = vector(0, 0, -1)
-    val light = point_light(point(0, 10, -10), color(1, 1, 1))
-  val result = lighting(m, light, position, eyev, normalv)
-  result shouldBe color(0.7364, 0.7364, 0.7364)
-}
+    @Test
+    fun `Scenario Lighting with eye opposite surface, light offset 45 deg`() {
+        val eyev = vector(0, 0, -1)
+        val normalv = vector(0, 0, -1)
+        val light = PointLight(point(0, 10, -10), color(1, 1, 1))
+        val result = m.lighting(light, position, eyev, normalv)
+        result shouldBe color(0.7364, 0.7364, 0.7364)
+    }
 
-      @Test
-      fun `Scenario Lighting with eye in the path of the reflection vector`() {
-  val eyev = vector(0, -√2/2, -√2/2)
-    val normalv = vector(0, 0, -1)
-    val light = point_light(point(0, 10, -10), color(1, 1, 1))
-  val result = lighting(m, light, position, eyev, normalv)
-  result shouldBe color(1.6364, 1.6364, 1.6364)
-}
+    @Test
+    fun `Scenario Lighting with eye in the path of the reflection vector`() {
+        val eyev = vector(0.0, -magic2, -magic2)
+        val normalv = vector(0, 0, -1)
+        val light = PointLight(point(0, 10, -10), color(1, 1, 1))
+        val result = m.lighting(light, position, eyev, normalv)
+        result shouldBe color(1.6364, 1.6364, 1.6364)
+    }
 
-      @Test
-      fun `Scenario Lighting with the light behind the surface`() {
-  val eyev = vector(0, 0, -1)
-    val normalv = vector(0, 0, -1)
-    val light = point_light(point(0, 0, 10), color(1, 1, 1))
-  val result = lighting(m, light, position, eyev, normalv)
-  result shouldBe color(0.1, 0.1, 0.1)
-}
+    @Test
+    fun `Scenario Lighting with the light behind the surface`() {
+        val eyev = vector(0, 0, -1)
+        val normalv = vector(0, 0, -1)
+        val light = PointLight(point(0, 0, 10), color(1, 1, 1))
+        val result = m.lighting(light, position, eyev, normalv)
+        result shouldBe color(0.1, 0.1, 0.1)
+    }
 
+    /*
       @Test
       fun `Scenario Lighting with the surface in shadow`() {
         val eyev = vector (0, 0, -1)
@@ -102,6 +94,19 @@ internal class MaterialTest {
           c1 shouldBe color (1, 1, 1)
           c2 shouldBe color (0, 0, 0)
         }
+
+      @Test
+      fun `Scenario Reflectivity for the default material`() {
+  val m = Material()
+  m.reflective shouldBe 0.0
+}
+
+      @Test
+      fun `Scenario Transparency and Refractive Index for the default material`() {
+  val m = Material()
+  m.transparency shouldBe 0.0
+    val m.refractive_index shouldBe 1.0
+}
 
  */
       }
