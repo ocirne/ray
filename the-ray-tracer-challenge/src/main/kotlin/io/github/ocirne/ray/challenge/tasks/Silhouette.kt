b9de1b464682488224f91250a70cc50c1f0d0d8a
Enrico Pilz <kontakt@enricopilz.de>
2021-06-29 20:59:27 +0200

    Variation on silhouette task

---
+++ the-ray-tracer-challenge/src/main/kotlin/io/github/ocirne/ray/challenge/tasks/Silhouette.kt
@@ -0,0 +1,45 @@
+package io.github.ocirne.ray.challenge.tasks
+
+import io.github.ocirne.ray.challenge.canvas.Canvas
+import io.github.ocirne.ray.challenge.raysphere.Ray
+import io.github.ocirne.ray.challenge.raysphere.Sphere
+import io.github.ocirne.ray.challenge.transformations.rotationZ
+import io.github.ocirne.ray.challenge.transformations.scaling
+import io.github.ocirne.ray.challenge.transformations.shearing
+import io.github.ocirne.ray.challenge.tuples.*
+import java.io.File
+import kotlin.math.PI
+
+
+fun main() {
+    val wallSize = 4.0
+    val canvasSize = 600
+    val middle = wallSize / 2
+    val canvas = Canvas(canvasSize, canvasSize)
+
+    val eye = point(0, 0, -20)
+    val spheres = listOf(
+        Sphere(),
+        Sphere(scaling(1.0, 0.5, 1.0).translate(-2.0, -2.0, 0.0)),
+        Sphere(scaling(0.5, 1.0, 1.0).translate(-2.0, 2.0, 0.0)),
+        Sphere(rotationZ(PI / 4).scale(0.5, 1.0, 1.0).translate(2.0, -2.0, 0.0)),
+        Sphere(shearing(1, 0, 0, 0, 0, 0).scale(0.5, 1.0, 1.0).translate(2.0, 2.0, 0.0))
+    )
+
+    val pixelSize = wallSize / canvasSize
+
+    for (x in 0 until canvasSize) {
+        for (y in 0 until canvasSize) {
+            val direction = vector(x * pixelSize - middle, y * pixelSize - middle, 10.0)
+            val ray = Ray(eye, direction)
+            val hit = spheres.map { sphere -> sphere.intersect(ray).isNotEmpty() }.any { it }
+            if (hit) {
+                canvas.writePixel(x, y, RED)
+            } else {
+                canvas.writePixel(x, y, BLACK)
+            }
+        }
+    }
+
+    File("output/silhouette.ppm").printWriter().use(canvas::toPPM)
+}
