eb99e1080be3f6a8aff8056a83300636f1fd92b7
Enrico Pilz <kontakt@enricopilz.de>
2021-07-02 12:27:23 +0200

    Implement cubes

--- the-ray-tracer-challenge/src/main/kotlin/io/github/ocirne/ray/challenge/shapes/Cube.kt
+++ the-ray-tracer-challenge/src/main/kotlin/io/github/ocirne/ray/challenge/shapes/Cube.kt
@@ -8,6 +8,7 @@ import io.github.ocirne.ray.challenge.raysphere.Ray
 import io.github.ocirne.ray.challenge.tuples.Point
 import io.github.ocirne.ray.challenge.tuples.Vector
 import io.github.ocirne.ray.challenge.tuples.vector
+import kotlin.math.abs
 
 data class Cube(
     override val transform: Matrix = identityMatrix,
@@ -15,10 +16,27 @@ data class Cube(
 ) : Shape(transform, material) {
 
     override fun localIntersect(localRay: Ray): List<Intersection> {
-        return listOf() // empty set--no intersections
+        val (xtmin, xtmax) = checkAxis(localRay.origin.x, localRay.direction.x)
+        val (ytmin, ytmax) = checkAxis(localRay.origin.y, localRay.direction.y)
+        val (ztmin, ztmax) = checkAxis(localRay.origin.z, localRay.direction.z)
+        val tmin = maxOf(xtmin, ytmin, ztmin)
+        val tmax = minOf(xtmax, ytmax, ztmax)
+        return if (tmin <= tmax) listOf(Intersection(tmin, this), Intersection(tmax, this)) else listOf()
+    }
+
+    private fun checkAxis(origin: Double, direction: Double): Pair<Double, Double> {
+        val tmin = (-1 - origin) / direction
+        val tmax = (1 - origin) / direction
+        return if (tmin > tmax) Pair(tmax, tmin) else Pair(tmin, tmax)
     }
 
     override fun localNormalAt(localPoint: Point): Vector {
-        return vector(0, 1, 0)
+        val maxc = maxOf(abs(localPoint.x), abs(localPoint.y), abs(localPoint.z))
+
+        return when (maxc) {
+            abs(localPoint.x) -> vector(localPoint.x, 0.0, 0.0)
+            abs(localPoint.y) -> vector(0.0, localPoint.y, 0.0)
+            else -> vector(0.0, 0.0, localPoint.z)
+        }
     }
 }
