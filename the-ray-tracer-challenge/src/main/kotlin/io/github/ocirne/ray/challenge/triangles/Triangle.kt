10a0cefeaa4d81a14ac00228bb647fa53810fe67
Enrico Pilz <kontakt@enricopilz.de>
2021-07-06 21:39:02 +0200

    Implement Triangles

--- the-ray-tracer-challenge/src/main/kotlin/io/github/ocirne/ray/challenge/triangles/Triangle.kt
+++ the-ray-tracer-challenge/src/main/kotlin/io/github/ocirne/ray/challenge/triangles/Triangle.kt
@@ -1,11 +1,13 @@
 package io.github.ocirne.ray.challenge.triangles
 
+import io.github.ocirne.ray.challenge.math.epsilon
 import io.github.ocirne.ray.challenge.raysphere.Intersection
 import io.github.ocirne.ray.challenge.raysphere.Ray
 import io.github.ocirne.ray.challenge.shapes.Bounds
 import io.github.ocirne.ray.challenge.shapes.Shape
 import io.github.ocirne.ray.challenge.tuples.Point
 import io.github.ocirne.ray.challenge.tuples.Vector
+import kotlin.math.abs
 
 class Triangle(val p1: Point, val p2: Point, val p3: Point): Shape() {
 
@@ -14,11 +16,28 @@ class Triangle(val p1: Point, val p2: Point, val p3: Point): Shape() {
     val normal: Vector = e2.cross(e1).normalize()
 
     override fun localIntersect(localRay: Ray): List<Intersection> {
-        TODO("Not yet implemented")
+        val dirCrossE2 = localRay.direction.cross(e2)
+        val det = e1.dot(dirCrossE2)
+        if (abs(det) < epsilon) {
+            return listOf()
+        }
+        val f = 1.0 / det
+        val p1ToOrigin = localRay.origin - p1
+        val u = f * p1ToOrigin.dot(dirCrossE2)
+        if (u < 0 || u > 1) {
+            return listOf()
+        }
+        val originCrossE1 = p1ToOrigin.cross(e1)
+        val v = f * localRay.direction.dot(originCrossE1)
+        if (v < 0 || (u + v) > 1) {
+            return listOf()
+        }
+        val t = f * e2.dot(originCrossE1)
+        return listOf(Intersection(t, this))
     }
 
     override fun localNormalAt(localPoint: Point): Vector {
-        TODO("Not yet implemented")
+        return normal
     }
 
     override fun bounds(): Bounds {
