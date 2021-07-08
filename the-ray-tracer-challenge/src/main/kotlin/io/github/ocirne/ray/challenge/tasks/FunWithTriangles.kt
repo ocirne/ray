28cc40e66a7c080d5fe76dc0f9cfb43010b547f2
Enrico Pilz <kontakt@enricopilz.de>
2021-07-08 19:56:07 +0200

    Correct sizing of triangles

--- the-ray-tracer-challenge/src/main/kotlin/io/github/ocirne/ray/challenge/tasks/FunWithTriangles.kt
+++ the-ray-tracer-challenge/src/main/kotlin/io/github/ocirne/ray/challenge/tasks/FunWithTriangles.kt
@@ -17,17 +17,20 @@ fun funWithTriangles1(): Canvas {
     val greenMaterial = Material(color= GREEN)
     val redMaterial = Material(color = RED)
 
+    val fx = sqrt(3.0)
+    val fy = 2.0
+
     val shapes = ArrayList<Shape>()
     for (x in 0..10) {
         for (y in 0..10) {
-            val bx = x * 2.0 - 10
-            val by = y * 2.0 - (x % 2) - 10
+            val bx = x * fx - 10
+            val by = y * fy - (x % 2) - 10
             val p1 = getPoint(bx, by)
-            val p2 = getPoint(bx+2, by+1)
-            val p3 = getPoint(bx, by+2)
+            val p2 = getPoint(bx+fx, by+fy*0.5)
+            val p3 = getPoint(bx, by+fy)
             shapes.add(Triangle(p1, p2, p3, greenMaterial))
 
-            val p4 = getPoint(bx+2, by+3)
+            val p4 = getPoint(bx+fx, by+fy*1.5)
             shapes.add(Triangle(p2, p3, p4, redMaterial))
         }
     }
