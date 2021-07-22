d1ddaf3c6e04193bbaa1a950474e71b1d055e850
Enrico Pilz <kontakt@enricopilz.de>
2021-07-22 20:57:54 +0200

    Minimal build.gradle for OpenGL

---
+++ learning-modern-3d-graphics-programming/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+	kotlin("jvm") version "1.5.10"
+}
+
+group = "io.github.ocirne.vulkan-test"
+version = "1.0-SNAPSHOT"
+
+repositories {
+	mavenCentral()
+}
+
+val lwjglVersion = "3.2.3"
+val lwjglNatives = "natives-linux"
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation(kotlin("stdlib"))
+
+	implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
+
+	implementation("org.lwjgl", "lwjgl")
+	implementation("org.lwjgl", "lwjgl-assimp")
+	implementation("org.lwjgl", "lwjgl-glfw")
+	implementation("org.lwjgl", "lwjgl-openal")
+	implementation("org.lwjgl", "lwjgl-opengl")
+	implementation("org.lwjgl", "lwjgl-stb")
+	runtimeOnly("org.lwjgl", "lwjgl", classifier = lwjglNatives)
+	runtimeOnly("org.lwjgl", "lwjgl-assimp", classifier = lwjglNatives)
+	runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
+	runtimeOnly("org.lwjgl", "lwjgl-openal", classifier = lwjglNatives)
+	runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
+	runtimeOnly("org.lwjgl", "lwjgl-stb", classifier = lwjglNatives)
+}
